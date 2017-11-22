db3619391b02cae03ee3a97ba1ad2853f9857c74
Brian van Rheenen <brian@moodles.nl>
2017-11-22 12:53:08 +0100

    Header has been redesigned. Icons have been replaced with a menu to allow more links in the future.

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,7 +4,6 @@
     <link rel="icon" type="image/png" href="images/favicon.ico" />
     <link rel="stylesheet" href="/css/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="/css/font-awesome/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link rel="stylesheet prefetch" href="https://cdn.rawgit.com/ItsMeKuroro/roboto/web/roboto.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="/css/{{ $css }}.css">
